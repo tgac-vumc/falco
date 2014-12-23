02beee0f60a83348d6566d3d20b46b6e320e911c
Daoud Sie <daoud.sie@gmail.com>
2014-12-23 18:28:19 +0100

    Removed commented code

--- lib/perl/perAmpliconAnalysis.pl
+++ lib/perl/perAmpliconAnalysis.pl
@@ -52,7 +52,6 @@ while (<M>) {
         s/\s+$//;
         my @row = split(/\t/, $_);
         push @{$targets{$row[3]}{$_}}, $row[0] foreach ($row[4] .. $row[5]);
-    #    $manif{$aTarget} = [@row];
 	my $aTarget = $row[0] . "-" . $row[3] . ":". "$row[4]-$row[5]";
         if (not exists($stats{$aTarget}{target})) {
                 $stats{$aTarget}{target} = [@row];
@@ -69,40 +68,23 @@ while (<M>) {
                 $stats{$aTarget}{assayStart} = $row[4] + length($probe{$row[0]}{ULSO});
                 $stats{$aTarget}{assayEnd} = $row[5] - length($probe{$row[0]}{DLSO});
         }
-#	print $row[0] . "\n";
 	foreach my $i (0 .. 5) {
 		foreach my $j (0 .. 5) {
-#			print $row[4] + $i .":". ($row[5] - $row[4] + $j) .":". $row[0] . "\n";
-#			print $row[4] + $i .":". ($row[5] - $row[4] - $j) .":". $row[0] . "\n";
-#			print $row[4] - $i .":". ($row[5] - $row[4] + $j) .":". $row[0] . "\n";
-#			print $row[4] - $i .":". ($row[5] - $row[4] - $j) .":". $row[0] . "\n";
 			$hash{$row[3]}{$row[4] + $i}{$row[5] - $row[4] + $j} = $aTarget;
 			$hash{$row[3]}{$row[4] + $i}{$row[5] - $row[4] - $j} = $aTarget;
 			$hash{$row[3]}{$row[4] - $i}{$row[5] - $row[4] + $j} = $aTarget;
 			$hash{$row[3]}{$row[4] - $i}{$row[5] - $row[4] - $j} = $aTarget;
 		}
 	}
-#	sleep 1;
 }
 close M;
-
-# samtools view -b 25MUT5.bam chr9:133738302-133738491 | samtools calmd - /gsdata/tmp/TSACP/ampliconPipeLine/ref/hg19/bwa-5/hg19.fa | head
-
-#foreach my $amp (keys(%stats)) {
-
-#	print $amp . "\n";
-#	my @SAM = ();
-	# Fetch reads
-#	my $coord = $stats{$amp}{chr} .":".$stats{$amp}{start}."-".$stats{$amp}{end};
-#	my $ln = $stats{$amp}{ln};
-#	print $coord . " $ln\n";
 
 # Open output 
 open QC, ">$base.qc.ann.txt";
 open QC2, ">$base.qc2.ann.txt";
 open TARGET, ">$base.qc.targets.txt";
 
-#TODO Remove stampy check
+# Stampy check
 my $stampy = 0;
 open SAMH, "$samtools view -H $bam |";
 while (<SAMH>) {
@@ -112,8 +94,6 @@ while (<SAMH>) {
 close SAMH;
 
 open SAM, "$samtools view $bam |";
-#open SAM, "$samtools view -b $bam | $samtools calmd - $ref |";
-#	open SAM, "$samtools view -b $bam $coord  | $samtools calmd - $ref |";
 while (<SAM>) {
 	last unless (/^@/);
 }
@@ -128,7 +108,6 @@ print QC2 "#" . join("\t", qw/read chr pos amp event/) . "\n";
 print TARGET "#" . join("\t", qw/amp chr start end assayStart assayEnd depth/) . "\n";
 while (<SAM>) {
 	# Skip stampy reads? Nope, stampy is no longer needed
-	#TODO Remove stampy check
 #	next if ($stampy == 1 && ! /XP:Z:BWA/);
 	chomp;
 	my @row = split(/\t/, $_);
@@ -137,13 +116,11 @@ while (<SAM>) {
 	$cig =~ s/(\d+)[MD]/$l+=$1/eg;
 		
 	my $amp = $hash{$row[2]}{$row[3]}{$l} || next;
-	#print "$row[2] $row[3] $l $amp\n"; 
 
 	push @{$stats{$amp}{sam}}, [@row];
 	$stats{$amp}{depth}++;
 	if (($pamp ne ".") && ($amp ne $pamp)) {
 		print STDOUT "Flushing $pamp $stats{$pamp}{depth}\n";
-#		print TARGET join("\t", $pamp, map { $stats{$pamp}{$_} } qw/chr start end assayStart assayEnd depth/) . "\n";
 		my $data = &call($stats{$pamp}{sam});
 		&flush($data, $stats{$pamp}{chr}, $pamp, $stats{$pamp}{assayStart}, $stats{$pamp}{assayEnd});
 		$stats{$pamp}{sam} = [];
@@ -163,21 +140,7 @@ foreach my $pamp (keys %stats) {
 		print TARGET join("\t", $pamp, map { $stats{$pamp}{$_} || "NA"} qw/chr start end assayStart assayEnd depth/) . "\n";
 }
 close TARGET;
-	
-
-#	print join("\n", map {$hsh{$_} . ":" . $_} keys(%hsh)) . "\n";
-#	print $cnt . "\n"; sleep 1;
-	close SAM;
-#	$stats{$amp}{depth} = $cnt;
-#	my $data = &call(\@SAM);
-#	&flush($data, $stats{$amp}{chr}, $amp);
-
-#}
-
-#foreach my $amp (keys(%stats)) {
-
-#	print $amp . ":" . scalar(@{$stats{$amp}{sam}}) . "\n";
-#}
+close SAM;
 
 sub call {
 	my $SAM = shift;
@@ -262,8 +225,6 @@ sub call {
 		}
 		$cnt++;
 	}
-	#my $thresh = scalar(@$SAM) * .05;
-	#&coocCalc(\%cooc, \%data, $thresh);	
 	return \%data;
 }
 
@@ -282,7 +243,6 @@ sub coocCalc {
 	}
 	my @muts = keys(%{$cooc});
 	foreach my $i (0 .. $#muts) {
-#		my %hsh = map { $_ => 0 } keys(%{$cooc{$muts[$i]}});
 		foreach my $j (($i + 1) .. $#muts) {
 			my $score = 0;
 			foreach my $m (keys(%{$cooc->{$muts[$i]}})) {
@@ -294,8 +254,6 @@ sub coocCalc {
 		}	
 	}		
 
-	#{read}{mutation}	
-	#{mutation}{read}	
 	sleep 1;
 }
 
@@ -308,21 +266,14 @@ sub flush {
         return unless (exists($data->{$chr}));
         print STDOUT "printing ... ";
 	my %reads = (); # read->pos->nt
-#        foreach my $p (sort {$a <=> $b} keys(%{$data->{$chr}})) {
-#        	unless (($p >=  $s) && ($p <= $e)) {
-#			next;	
-#		}
 	foreach my $p ($s .. $e) {
 	        my $posdat = $data->{$chr}{$p} || next;
 	        next unless (exists($posdat->{DP}));
                 my $refNT = uc($posdat->{refNT});
                 my @varOrd = sort { $posdat->{NT}{$b} <=> $posdat->{NT}{$a} } grep {$_ ne "N"} grep { $_ ne $refNT  } keys(%{$posdat->{NT}});
                 my $seqNT = join("/", @varOrd);
-                #my $refCNT = scalar(@{$posdat->{NT}{$refNT} || []});
 		my $refCNT = $posdat->{NT}{$refNT} || 0;
 		my $varCNT = 0;
-		#$varCNT += scalar(@{$posdat->{NT}{$_}}) foreach (@varOrd);
-		#$varCNT += $posdat->{NT}{$_} foreach (@varOrd);
 		$varCNT = ($varOrd[0])?$posdat->{NT}{$varOrd[0]}:0;
 		my $Istr = ".";
 		my $Dstr = ".";
@@ -341,16 +292,13 @@ sub flush {
 				$reads{$_}{$p} = "$D/$refNT" foreach (@{$posdat->{DR}{$D}});
 			}
 		}
-		#$Dstr = join("|", map { $_ . ":" . $posdat->{D}{$_} } keys(%{$posdat->{D}})) if exists($posdat->{D});
 		print QC join("\t", $chr, $p, $amp,
 				$posdat->{DP} || 0,
 				$posdat->{refNT} || ".",
 				$seqNT || ".",
 				$refCNT, $varCNT,
-				#(map { scalar(@{$posdat->{NT}{$_} || []})} qw(A C G T N)), $Istr, $Dstr,
 				(map { $posdat->{NT}{$_} || 0 } qw(A C G T N)), $Istr, $Dstr,
 			  ) . "\n";
-		# delete $data->{$chr}{$p};
 	}
 	delete $data->{$chr};
 	foreach my $r (sort { $a <=> $b } keys(%reads)) {
